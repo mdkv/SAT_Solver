c89d3a5a75579782574e5613c87a1aad342770c5
mdkv <79453726+mdkv@users.noreply.github.com>
2021-11-16 11:06:54 +0100

    Updated the code for unit and simplify
    Updated the code for unit and simplify

--- main.py
+++ main.py
@@ -49,87 +49,107 @@ def create_dict(constraints):
 
     return variables
 
-def remove_redundant(cnf, literals):
+def remove_redundant(cnf, literal):
 
     new_cnf = deepcopy(cnf)
 
-    num_deleted = 0 #This keeps track of how many clauses are deleted, so that index is adjusted in new_cnf
+    i = 0
+    while i < len(new_cnf):
 
-    for literal in literals:
-        for i in range(len(cnf)):
 
-            if literal in cnf[i]: #If there is an exact match, the clause is True, so remove it
-                new_cnf.pop(i - num_deleted)
-                num_deleted +=1
+        if literal in new_cnf[i]: #If there is an exact match, the clause is True, so remove it
+            del new_cnf[i]
 
-            elif -literal in cnf[i]: #If the negated form is present, the clause can't be satisfied with that literal
-                new_cnf[i-num_deleted].remove(-literal)
+        else:
+            if -literal in new_cnf[i]: #If the negated form is present, the clause can't be satisfied with that literal
+                new_cnf[i].remove(-literal)
 
+            i +=1
 
     return new_cnf
 
 
 def set_unit_clauses(cnf, variables):
+    print("here at unit")
 
-    units = []
-
-    for i in range(len(cnf)):
-        if len(cnf[i]) == 1: #Length 1 means clause has 1 literal, thus it is a unit clause
+    i = 0 #to do check if i = 0  in one of the if statements can be removed
+    while i < len(cnf):
+        if len(cnf[i]) == 1: #Length 1 means clause has 1 literal, thus it is a unit claus
             if cnf[i][0] > 0: #If literal is positive, set variable to true
                 variables[abs(cnf[i][0])] = True
+                cnf = remove_redundant(cnf, cnf[i][0])
+                i = 0
+
 
             else: #If literal is negated, set variable to false
                 variables[abs(cnf[i][0])] = False
+                cnf = remove_redundant(cnf, cnf[i][0])
+                i =0
 
-            units.append(cnf[i][0])
+        i+=1
 
-
-    cnf = remove_redundant(cnf, units)
 
 
     return cnf, variables
 
-def set_initial_sudoku(variables, sudoku):
+def initialize_sudoku(cnf, variables, sudoku):
 
     for position in sudoku:
         variables[position[0]] = True
+        cnf = remove_redundant(cnf, position[0])
 
-    return variables
+    return cnf, variables
 
 def check_satisfiable(cnf):
 
     if len(cnf) == 0: #All the clauses are removed, thus it is satisfied
-        return 'SAT'
+        return True
 
     if len(cnf) !=0: #If there are still clauses in the cnf
-        print("here")
         for clause in cnf:
             if len(clause) == 0: #If the clause does exists, and it is empty, it can't be satisfied
-                return 'UNSAT'
+                return False
 
         return 'UNFIN' #Else the cnf can still be satisfied with remaining clauses
 
 
 def dpll(cnf, variables):
+    print("here")
 
-    #Pick the first variable which is not set
-    #Code from: https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
+    current_variables = deepcopy(variables)  # Copy the variables, so previous don't get changed
 
+    new_cnf, current_variables = set_unit_clauses(cnf, current_variables)  # Check for unit clauses
 
-    key_list = list(variables.keys()) #Get all the keys
-    val_list = list(variables.values()) #Get all the values
-    position = val_list.index(None) #Get the index of the first unset variable
+    satisfiable = check_satisfiable(cnf)
+    #print(satisfiable)
 
-
-    variables[position] = True #First try True
-
-    cnf = remove_redundant(cnf, key)
-
+    # If this returns either True or False return that value back and either stop or try False. Else try next literal T
+    if satisfiable != "UNFIN":
+        return satisfiable, current_variables
 
 
+    # Pick the first variable which is not set
+    # Code from: https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
+    key_list = list(current_variables.keys()) #Get all the keys
+    val_list = list(current_variables.values()) #Get all the values
+    index = val_list.index(None) #Get the index of the first unset variable
+    position = key_list[index]
+    print(position)
 
 
+    #First try True
+    current_variables[position] = True
+    new_cnf = remove_redundant(cnf, position)
+    satisfiable, variables = dpll(new_cnf, variables)
+    if satisfiable == True:
+        print("Trying True on: " + str(position))
+        return satisfiable, variables
 
+    #Now try False, hence the negated position
+    current_variables[position] = False
+    new_cnf = remove_redundant(cnf, -position)
+    print("Trying false on: " + str(position))
+    return dpll(new_cnf, variables)
 
 
 
@@ -137,6 +157,7 @@ if __name__ == '__main__':
     cnf = read_constraints(PATH_TO_CONSTRAINTS)
     sudoku = read_sudoku(PATH_TO_SUDOKU)
     variables = create_dict(cnf)
-    variables = set_initial_sudoku(variables, sudoku)
-    cnf, variables = set_unit_clauses(cnf, variables)
-    dpll(cnf,variables)
+    cnf, variables = initialize_sudoku(cnf, variables, sudoku)
+    satisfiable, current_variables = dpll(cnf,variables)
+    print("SAT: " + str(satisfiable))
+    print(current_variables)
